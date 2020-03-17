96ccc325e9586f0b86a83019fc24fd8afb2acb6e
fbzr <fabricio.spb@gmail.com>
2020-03-17 15:14:02 -0700

    add useDispatch hook to add a feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addFeature } from '../actions/car';
 
-const AdditionalFeature = ({ feature, addFeature }) => {
+const AdditionalFeature = ({ feature }) => {
+  const dispatch = useDispatch();
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button onClick={() => addFeature(feature)} className="button">Add</button>
+      <button onClick={() => dispatch(addFeature(feature))} className="button">Add</button>
       {feature.name} (+{feature.price})
     </li>
   );
 };
 
-export default connect(null, { addFeature })(AdditionalFeature);
+export default AdditionalFeature;
